2c59fd7225b5d1fccb9f0c0ff58198a71402992f
vitayou <vitaminyougoon@gmail.com>
2022-02-21 18:28:28 +0900

    🚧 Implement deleting all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,6 +1,11 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearch.querySelector('ol')
+
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
 
 function closeGnbSearchHistory(e) {
   if (!gnbSearch.contains(e.target)) {
@@ -10,6 +15,13 @@ function closeGnbSearchHistory(e) {
 }
 
 function openGnbSearchHistory() {
+  // 체크 => gnbSearchHistoryList 안에 li가 몇개가 있는지
+  // li가 0개 => 실행 x
+
+  if (gnbSearchHistoryList.children.length === 0) {
+    return
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistory)
   }
@@ -17,3 +29,11 @@ function openGnbSearchHistory() {
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+function deleteAllSearchHistoryItems() {
+  // gnbSarchHistoryList 안에 들어있는 모든 li를 삭제해!
+  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+}
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems)
