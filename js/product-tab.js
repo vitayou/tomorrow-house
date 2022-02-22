84cd1fea0b7ae80afb961f9646fb49e22a6d2364
vitayou <vitaminyougoon@gmail.com>
2022-02-23 03:20:00 +0900

    🚧 Implement updating Update Tab

--- js/product-tab.js
+++ js/product-tab.js
@@ -61,9 +61,41 @@ function detectTabPanelPosition() {
     const position = window.scrollY + panel.getBoundingClientRect().top
     productTabPanelPositionMap[id] = position
   })
+}
 
-  console.log(productTabPanelPositionMap)
+function updateActiveTabOnScroll() {
+  // 스크롤 위치에 따라서 activeTab을 업데이트
+  // 1. 현재 유저가 얼마만큼 스크롤을 했느냐 -> window.scrollY
+  // 2. 각 tabPanel y축 위치 -> productTabPanelPositionMap
+
+  const scrolledAmount =
+    window.scrollY +
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP + 80 : TOP_HEADER_MOBILE + 8)
+
+  let newActiveTab
+  if (scrolledAmount >= productTabPanelPositionMap['product-recommandation']) {
+    newActiveTab = productTabButtonList[4] // 추천 버튼
+  } else if (scrolledAmount >= productTabPanelPositionMap['product-shipment']) {
+    newActiveTab = productTabButtonList[3] // 배송/환불 버튼
+  } else if (scrolledAmount >= productTabPanelPositionMap['product-inquiry']) {
+    newActiveTab = productTabButtonList[2] // 문의 버튼
+  } else if (scrolledAmount >= productTabPanelPositionMap['product-review']) {
+    newActiveTab = productTabButtonList[1] // 리뷰 버튼
+  } else {
+    newActiveTab = productTabButtonList[0] // 상품정보 버튼
+  }
+
+  if (newActiveTab) {
+    newActiveTab = newActiveTab.parentNode
+
+    if (newActiveTab !== currentActiveTab) {
+      newActiveTab.classList.add('is-active')
+      currentActiveTab.classList.remove('is-active')
+      currentActiveTab = newActiveTab
+    }
+  }
 }
 
 window.addEventListener('load', detectTabPanelPosition)
 window.addEventListener('resize', detectTabPanelPosition)
+window.addEventListener('scroll', updateActiveTabOnScroll)
