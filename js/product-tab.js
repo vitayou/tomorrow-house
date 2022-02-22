ed381db5a86c6833da77dd298e9f63a2130c730a
vitayou <vitaminyougoon@gmail.com>
2022-02-22 11:09:34 +0900

    💫 Scroll to tab-panel when product-tab-item click

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,6 +1,9 @@
 const productTab = document.querySelector('.product-tab')
 const productTabButtonList = productTab.querySelectorAll('button')
 
+const TOP_HEADER_DESKTOP = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
+
 let currentActiveTab = productTab.querySelector('.is-active')
 
 function toggleActiveTab() {
@@ -11,10 +14,23 @@ function toggleActiveTab() {
     tabItem.classList.add('is-active')
     currentActiveTab.classList.remove('is-active')
     currentActiveTab = tabItem
-    console.log(currentActiveTab)
   }
 }
 
+function scrollToTabPanel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabPanel = document.querySelector(`#${tabPanelId}`)
+
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
+
+  window.scrollBy({
+    top: scrollAmount,
+    behavior: 'smooth',
+  })
+}
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab)
+  button.addEventListener('click', scrollToTabPanel)
 })
